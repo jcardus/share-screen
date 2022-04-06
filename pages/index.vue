6f0a123d5b5d3bac497ab6cf9f8a3fadcfe8852e
joaquimcardeira <joaquim_cardeira@hotmail.com>
2022-04-06 03:30:46 +0100

    starting share screen

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,17 @@
 <template>
-  <Tutorial />
+  <div>
+    <video ref="video" />
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  async mounted () {
+    this.$refs.video.srcObject = await navigator.mediaDevices.getDisplayMedia({
+      video: { cursor: 'always' },
+      audio: false
+    })
+  }
 }
 </script>
